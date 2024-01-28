9ff402a6433ea53138cd23abd1e420d9738bf44e
Ajish <ajish.vnair@neoito.com>
2024-01-28 11:00:39 +0530

    feat: first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,20 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import kartonmedya from './kartonmedya.png'
+import verisoft from './verisoft.png'
+import okttrailer from './okttrailer.png'
+import logo from './logo.svg'
+import neoito from './neoito.png'
+import helixo from './helixo_logo.jpeg'
+
+
+export {
+    hero,
+    meta,
+    kartonmedya,
+    verisoft,
+    okttrailer,
+    logo,
+    neoito,
+    helixo,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,190 @@
+import { neoito, helixo } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+  aasani
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Technical Team Lead",
+    company_name: "Neoito",
+    icon: neoito,
+    iconBg: "#accbe1",
+    date: "Nov 2020 - Jan 2024",
+    points: [
+      "As a dynamic technical team lead overseeing the development of a cutting-edge financial product, I am dedicated to delivering exceptional solutions to our clients without ever compromising on quality.",
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full-Stack Developer",
+    company_name: "Helixo",
+    icon: helixo,
+    iconBg: "#c0c0c0",
+    date: "Jan 2020 - Nov 2020",
+    points: [
+      "As a proficient full-stack developer, I specialized in crafting bespoke Shopify plugins tailored to meet the unique needs of e-commerce retailers, ensuring the delivery of top-tier solutions that enhance their online storefronts.",
+      "Demonstrating effective communication and project management skills during the analysis, testing and development process to ensure the successful outcome of the ERP transformation project.",
+      "Actively working with business teams throughout this process."
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/ajishvnair-neoito",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/ajishv/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: aasani,
+    theme: "btn-back-blue",
+    name: "FinTech product",
+    description: "Streamline your business's accounting processes with our AI-powered tool, making it faster and easier than ever before. Say goodbye to hassles and hello to efficiency as you manage your finances effortlessly.",
+    link: "https://www.aasanibooks.com/",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "An AI Article Summarizer Website",
+    description: "Summarize Articles with OpenAI GPT-4. Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.",
+    link: "https://github.com/emredkyc/ai_summarizer",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "A Car Showcase Website",
+    description: "Build and Deploy a Modern Next.js 14 Application | React, Next JS 14, TypeScript, Tailwind CSS",
+    link: "https://github.com/emredkyc/car_showcase",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-yellow",
+    name: "Personal portfolio website.",
+    description:
+      "Personal portfolio website.",
+    link: "https://emredkyc.vercel.app",
+  },
+];
